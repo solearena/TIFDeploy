ae7c6e23f312ace8a24ad82368c2cd2b2027d48e
solearena <67062318+solearena@users.noreply.github.com>
2023-07-09 21:20:41 -0300

    Update settings.py

--- proyectoFinal/proyectoFinal/settings.py
+++ proyectoFinal/proyectoFinal/settings.py
@@ -124,11 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR /'static',
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root', 'static')
-
+#STATICFILES_DIRS = [
+#    BASE_DIR /'static',
+#]
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static_root', 'static')
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
